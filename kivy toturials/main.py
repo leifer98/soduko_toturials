72d93f8568e964a1fb1360ba3bfc2c81684d105f
matro68 <ofriofriofri@gmail.com>
2021-04-20 16:02:16 +0300

    socket files

--- kivy toturials/main.py
+++ kivy toturials/main.py
@@ -4,6 +4,8 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.clock import Clock
 import os
 
 kivy.require('2.0.0')
@@ -52,12 +54,43 @@ class ConnectPage(GridLayout):
         with open("details.txt",'w') as f:
             f.write(f"{ip},{port},{username}")
 
+        info = f"Attempting to join {ip}:{port} as {username}"
+        chat_app.info_page.update_info(info)
+
+        chat_app.screen_manager.current = 'Info'
+
+class InfoPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.message = Label(halign = 'center', valign = 'middle', font_size = 30)
+        self.message.bind(width = self.update_text_width)
+        self.add_widget(self.message)
+
+    def update_info(self, message):
+        self.message.text = message
+
+    def update_text_width(self, *_):
+        self.message.text_size = (self.message.width * 0.9, None)
 
 
 class EpicApp(App):
     def build(self):
-        return ConnectPage()
+        self.screen_manager = ScreenManager()
+
+        self.connect_page = ConnectPage()
+        screen = Screen(name = "Connect")
+        screen.add_widget(self.connect_page)
+        self.screen_manager.add_widget(screen)
+
+        self.info_page = InfoPage()
+        screen = Screen(name = "Info")
+        screen.add_widget(self.info_page)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
 
 if __name__ == '__main__':
-    EpicApp().run()
+    chat_app = EpicApp()
+    chat_app.run()
 

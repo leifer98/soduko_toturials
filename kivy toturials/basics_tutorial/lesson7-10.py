af485b498dd023dcac70acde4c367ab7c94c17ec
matro68 <ofriofriofri@gmail.com>
2021-04-26 02:08:26 +0300

    final one

--- kivy toturials/basics_tutorial/lesson7-10.py
+++ kivy toturials/basics_tutorial/lesson7-10.py
@@ -1,20 +1,37 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Line
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
 kivy.require('2.0.0')
 
-class TouchInput(Widget):
+class MainScreen(Screen):
+    pass
+
+class AnotherScreen(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+class DrawInput(Widget):
     def on_touch_down(self, touch):
-        print(touch)
+        # print(touch)
+        with self.canvas:
+            touch.ud['line'] = Line(points=(touch.x,touch.y))
     def on_touch_move(self, touch):
-        print(touch)
+        # print(touch)
+        touch.ud['line'].points += (touch.x,touch.y)
     def on_touch_up(self, touch):
         print(touch)
 
-class SimpleKivy(App):
+presenataion = Builder.load_file('Main.kv')
+
+class MainApp(App):
     def build(self):
-        return TouchInput()
+        return presenataion
 
 if __name__=='__main__':
-    SimpleKivy().run()
+    MainApp().run()
